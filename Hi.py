936c1f9dea8bdd7e01955c44049b9a1bdd3583bc
Rohanhub <nathiya2919@gmail.com>
2026-03-28 16:26:34 -0500

    Add files via upload

---
+++ Hi.py
@@ -0,0 +1,12 @@
+alice = "12/5/2010"
+ben = "4/1/2015"
+yuvan = "19/6/2017"
+dan = "28/3/2016"
+noah = "15/8/2014"
+
+print("Alice's Birthdate: ", alice)
+print("Ben's Birthdate: ", ben)
+print("Yuvan's Birthdate: ", yuvan)
+print("Dan's Birthdate ", dan)
+print("Noah's Birthdate ", noah)
+ 
