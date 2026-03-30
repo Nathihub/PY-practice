8f1051ab2242581dc89899c372db981fb86504b5
Rohanhub <nathiya2919@gmail.com>
2026-03-30 16:52:56 -0500

    Add files via upload

---
+++ o.py
@@ -0,0 +1,6 @@
+List = ["A,", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] + ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+R = (input("Enter a character: "))
+if (R in List):
+    print(R,"is in the Alphabet.")
+else:
+     print(R,"is not in the Alphabet.")
