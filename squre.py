e8831f3db5e8e07622107f8e2b1d1b6db1aea615
Rohanhub <nathiya2919@gmail.com>
2026-04-26 11:49:17 -0500

    Add files via upload

---
+++ squre.py
@@ -0,0 +1,19 @@
+import turtle
+turtle.Screen().bgcolor("light blue")
+turtle.Screen().setup(400, 400)
+turtle.Screen().title("Turtle")
+square = turtle.Turtle()
+
+square.forward(100)
+square.left(90)
+
+square.forward(100)
+square.left(90)
+
+square.forward(100)
+square.left(90)
+
+square.forward(100)
+square.left(90)
+
+turtle.done()
