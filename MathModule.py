7f23dd54b5bd5f90e54a579a0075426f1b8905c6
Rohanhub <nathiya2919@gmail.com>
2026-05-31 11:45:38 -0500

    Add files via upload

---
+++ MathModule.py
@@ -0,0 +1,16 @@
+import math
+print(math.sqrt(4))
+
+print(math.ceil(2))
+
+print(math.copysign(3, -2))
+
+print(math.factorial(7))
+
+print(math.floor(5))
+
+print(math.isnan(3))
+
+print(math.exp(2))
+
+print(math.degrees(1.57))
