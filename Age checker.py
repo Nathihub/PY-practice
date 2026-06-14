b345c694048acd5b2424fb1429c0c39e0b5749df
Rohanhub <nathiya2919@gmail.com>
2026-06-14 11:55:10 -0500

    Add files via upload

---
+++ Age checker.py
@@ -0,0 +1,5 @@
+Age = int(input("Enter your age: "))
+if Age >= 10 and Age <= 20:
+    print("Your are allowed to join Raj's class")
+else:
+    print("Sorry, you are not allowed to join Raj's class")
