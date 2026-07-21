b716f81e2689738add4cbe3240b6817249307ce9
Rohanhub <nathiya2919@gmail.com>
2026-07-21 10:20:48 -0500

    Add files via upload

---
+++ reversedstring.py
@@ -0,0 +1,9 @@
+string = input("Please enter your own string to reverse: ")
+
+string2 = ""
+
+for i in string:
+    string2 = i + string2
+
+print("\nThis is the string that you entered: ", string)  
+print("This is the reversed string:R ", string2) 
